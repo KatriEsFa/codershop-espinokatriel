00d937fbbe4869420a838f2dd599f66190975646
KatriEsFa <katriesfa@gmail.com>
2022-05-14 19:14:33 -0300

    Se agrego el navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,8 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import logomini from './assets/imgs/logomini.png';
 
 const Navbar = () => {
     return (
-        <div>Soy el Navbar</div>
+        <div className='navFather'>
+            <img className='logo' src={logomini} alt='logo'></img>
+            <nav>
+                <ul className='navBarLinks'>
+                    <li className='navLinksLi'><a href='#' className='navLink'>Servicios</a></li>
+                    <li className='navLinksLi'><a href='#' className='navLink'>Productos</a></li>
+                    <li className='navLinksLi'><a href='#' className='navLink'>Nosotros</a></li>
+                </ul>
+            </nav>
+            <a className='ctaUs' href='#'><button className='ctaUsBtn'>Contáctanos</button></a>
+        </div>
     );
 }
 
