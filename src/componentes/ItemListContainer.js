5b84dc12844c8d6883e56505625a62d274c68ca3
KatriEsFa <katriesfa@gmail.com>
2022-06-14 17:24:41 -0300

    Se intenta resolver problema de mostrar datos por categoría

--- src/componentes/ItemListContainer.js
+++ src/componentes/ItemListContainer.js
@@ -1,30 +1,90 @@
 import { useEffect, useState } from 'react';
 import ItemList from './ItemList';
 import { useParams } from 'react-router-dom';
-import { firestoreFetch } from '../utils/firestoreFetch';
+import db from '../utils/firebaseConfig';
+import { getDocs, collection, query, where } from 'firebase/firestore';
+import Item from './Item';
+import { Link } from 'react-router-dom';
+
+// import { firestoreFetch } from '../utils/firestoreFetch';
 
 const ItemListContainer = () => {
 
     const [datos, setDatos] = useState([]);
-    const { idCategory } = useParams();
+    const { category } = useParams(); //debe llevar el mismo nombre que la prop en firbase
+    const [loading, setLoading] = useState(true);
 
 
-
-    useEffect(() => {
-        firestoreFetch(idCategory)
-            .then(result => setDatos(result))
-            .catch(err => console.log(err));
-    }, [idCategory]);
-
-    useEffect(() => {
-        return (() => {
-            setDatos([]);
+    const getItems = async () => {
+        const productsCollection = collection(db, 'ItemCollection');
+        const itemsDoc = await getDocs(productsCollection);
+        const itemsList = itemsDoc.docs.map((item) => {
+            let item1 = item.data();
+            item1.id = item.id;
+            return item1;
         })
-    }, []);
+        return itemsList
+    };
+
+    const itemsFilter = (arrayItems, category) => {
+        arrayItems.map((e) => {
+            if (category.id === e.category) {
+                return setDatos(datos => [...datos, e])
+            }
+        })
+    };
+
+    useEffect(() => {
+        setLoading(true)
+        setDatos([]);
+        return getItems()
+            .then((result) => {
+                setLoading(false);
+                category
+                    ? itemsFilter(result, category)
+                    : setDatos(result)
+            })
+    }, [])
+
+
+
+
+
+    // useEffect(() => {
+    //     firestoreFetch(idCategory)
+    //         .then(result => setDatos(result))
+    //         .catch(err => console.log(err));
+    // }, [idCategory]);
+
+    // useEffect(() => {
+    //     return (() => {
+    //         setDatos([]);
+    //     })
+    // }, []);
 
     return (
         <>
-            <ItemList arrayObjetos={datos} />
+            {
+                !loading
+                    ? (
+
+                        datos.map((e) => {
+                            if (category) {
+                                return (
+                                    <Link to={`./${e.id}`}>
+                                        <Item id={e.id} title={e.title} price={e.price} stock={e.stock} itemImg={e.itemImg} description={e.description} />
+                                    </Link>
+                                );
+                            } else {
+                                return (
+                                    <ItemList arrayObjetos={datos} />
+                                )
+                            }
+                        })
+                    )
+                    : <div>loading</div>
+            }
+
         </>
     );
 }
